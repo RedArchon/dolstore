9dcb8f4c845830ca3791f7eaf704b1e69ed27e67
Vincent Hall <jack@olyjs.com>
2020-01-28 06:41:52 -0800

    category model and migration

--- app/Product.php
+++ app/Product.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $guarded = [];
-    //
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
